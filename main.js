0ea7fecc26c725dc8a5956588a4b091859fb4e9a
harshbatman <kumarmahtoharsh7@gmail.com>
2026-01-01 23:47:36 +0530

    Initial commit of Mine by MAHTO landing page

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+console.log('Mine by MAHTO loaded.');
